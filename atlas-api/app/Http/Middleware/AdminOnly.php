f5cf3b18119eb0420efc0f4316931610dfadbc89
Ni-salas <nicolas.salas.contacto@gmail.com>
2026-02-09 10:46:25 -0300

    Update: Rutas Admin

---
+++ atlas-api/app/Http/Middleware/AdminOnly.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminOnly
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user() || $request->user()->role->name !== 'admin') {
+            return response()->json([
+                'message' => 'Acceso denegado. Se requieren privilegios de Administrador.'
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
--- atlas-api/bootstrap/app.php
+++ atlas-api/bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\CheckMaintenanceMode;
+use App\Http\Middleware\AdminOnly;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
             CheckMaintenanceMode::class,
+        ]);
+        $middleware->alias([
+            'admin' => AdminOnly::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
