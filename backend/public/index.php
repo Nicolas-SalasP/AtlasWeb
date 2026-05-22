7c3042779f71a171ce4c8da461428e066a8a7267
Nicolas-Salas <nicolas.salas.contacto@gmail.com>
2026-05-21 23:53:00 -0400

    Fix (Deploy): index.php con basePath dinamico y workflow completo con tests y api.tenri.cl

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,20 +1,20 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-$basePath = __DIR__.'/../';
-if (str_contains(__DIR__, 'public_html')) {
-    $basePath = '/home/atlasdig/atlas_project/';
-}
+$basePath = match(true) {
+    str_contains(__DIR__, '/home/atlasdig') => '/home/atlasdig/tenri_backend',
+    default => __DIR__.'/../',
+};
 
-if (file_exists($maintenance = $basePath.'storage/framework/maintenance.php')) {
+if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-require $basePath.'vendor/autoload.php';
-$app = require_once $basePath.'bootstrap/app.php';
+require $basePath.'/vendor/autoload.php';
+
+$app = require_once $basePath.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
